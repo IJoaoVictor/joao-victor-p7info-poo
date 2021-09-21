23d5ba105be961f1ab7b35127e7562ad8641a912
João Victor <89043251+IJoaoVictor@users.noreply.github.com>
2021-09-21 19:08:01 -0300

    Atualizado em 21/09/2021

---
+++ atividade-presenca/Presença-02/conversor.py
@@ -0,0 +1,20 @@
+def printDecimal(num):
+    return str(num)
+
+def printOctal(num):
+    return str(oct(num)[2:])
+
+def printHexadecimal(num):
+    return str(hex(num)[2:])
+
+def printBinário(num):
+    return str(bin(num)[2:])
+
+def imprimirTabela():
+    for count in range(226):
+        print("Decimal       Octal       Hexadecimal         Binário    ")
+        print("-------      -------     -------------      -----------")
+        print("{:14s} {:14s} {:14s} {:s} \n" .format(printDecimal(count),printOctal(count),printHexadecimal(count),printBinário(count)))
+
+imprimirTabela()
+
