ad962c3138ebb2b036a693960aaa92c4d35c86ae
João Victor <89043251+IJoaoVictor@users.noreply.github.com>
2021-12-03 21:42:05 -0300

    Update __init__.py

--- atividade-avaliação/Avaliação-06/Database/__init__.py
+++ atividade-avaliação/Avaliação-06/Database/__init__.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Database.db') #'sqlite:///C:\\Users\\SAMSUNG\\Desktop\\Avaliação-06\\Database\\Database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Database.db')
 app.debug = True
 
 db = SQLAlchemy(app)
