866e3d6c64c2f62e52fd7b8009291c12befbc0ca
João Victor <89043251+IJoaoVictor@users.noreply.github.com>
2021-12-03 21:43:32 -0300

    Adicionado em 03/12/2021

--- atividade-avaliação/Avaliação-06/Database/__init__.py
+++ atividade-avaliação/Avaliação-06/Database/__init__.py
@@ -1,10 +1,11 @@
-import os.path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os.path
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'Database.db')
 app.debug = True
